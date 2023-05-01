1f66e2230fe8f5cc37f66c5d8bcc15c09bc0f862
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-05-01 17:35:02 -0300

    :bug: fix: corrigindo estrutura das envs

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const allRoutes = require('./routes');
 const app = express();
 swagger(app);
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 
 app.get('/', (_req, res) => {
