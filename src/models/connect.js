8635e802866836e27c0190394680ceb3906038f9
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-05-01 03:59:02 -0300

    :bug: fix: connect mongo

--- src/models/connect.js
+++ src/models/connect.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const { env: { DB_HOST, DB_PORT, DB_NAME } } = process;
-
+// const DB_HOST = 'AndreHorman'
+// const DB_PORT = '@Nerd19942@cluster0.x1q4u2y.mongodb.net/?retryWrites=true&w=majority'
 const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToMongoDB() {
