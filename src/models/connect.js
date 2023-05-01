a6eced9701b02d72660043f23eecaf68a0fe2723
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-05-01 17:39:43 -0300

    :bug: fix: corrigindo estrutura do connect

--- src/models/connect.js
+++ src/models/connect.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
+const { env } = process;
 const {
-  env: {
-    DB_HOST, DB_PORT, DB_NAME, MONGO_URL,
-  },
-} = process;
+  DB_HOST = 'localhost',
+  DB_PORT = '27017',
+  DB_NAME = 'mydatabase',
+  MONGO_URL,
+} = env;
 
-// const railwayMongoURI = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`;
 const URI = MONGO_URL || `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 const client = new MongoClient(
